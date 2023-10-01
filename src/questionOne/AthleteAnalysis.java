e2f909cdecb4e0fe9d7ac719991f1d1b720941d9
Almasri, Abdullah <a.almasri@hw.ac.uk>
2023-10-01 14:50:29 +0000

    CW

---
+++ src/questionOne/AthleteAnalysis.java
@@ -0,0 +1,15 @@
+package questionOne;
+
+/**
+ * F28PA | Software Development A | Coursework
+ * 
+ * The Coursework specification is provided in Canvas. Please read through it in
+ * full before you start work.
+ * 
+ * @author INSERT YOUR NAME HERE
+ */
+public class AthleteAnalysis {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/questionTwo/BookingSoft.java
@@ -0,0 +1,28 @@
+package questionTwo;
+
+/**
+ * F28PA | Software Development A | Coursework
+ * 
+ * The Coursework specification is provided in Canvas. Please read through it in
+ * full before you start work.
+ * 
+ * @author INSERT YOUR NAME HERE
+ */
+public class BookingSoft {
+	public static void main(String[] args) throws InterruptedException {
+		// There are currently 6 shows offered in 6 different venues.
+		Show[] shows = new Show[6];
+		
+		// Uncomment lines 17 to 22 once the constructors have been written.
+//		shows[0] = new Show("1N", new Film("SING", 1));
+//		shows[1] = new Show("2W", new Film("THE GRINCH", 2));
+//		shows[2] = new Show("3E", new Film("BOSS BABY", 3));
+//		shows[3] = new Show("3S", new Film("YES DAY", 3));
+//		shows[4] = new Show("1E", new Film("THE KARATE KID", 1));
+//		shows[5] = new Show("2N", new Film("THE SEA BEAST", 2));
+		System.out.println("### Welcome to the Booking System ###\n");
+		// DO NOT CHANGE THE ABOVE PART OF THE CODE.
+		///////////////////////////////////////////////////////////////////////////////////
+
+	}
+}
