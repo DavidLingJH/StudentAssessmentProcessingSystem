c143a452ed4bf6a4130b8a0f3c4ecf8d5951ab7c
Basuki, Thomas <t.basuki@hw.ac.uk>
2024-09-29 01:24:58 +0000

    Update AssessmentRecord.java

--- src/F28PAAssignment2024/AssessmentRecord.java
+++ src/F28PAAssignment2024/AssessmentRecord.java
@@ -1,5 +1,14 @@
 package F28PAAssignment2024;
 
+/**
+ * F28PA | Software Development A | Coursework
+ * 
+ * The Coursework specification is provided in Canvas. Please read through it in
+ * full before you start work.
+ * 
+ * @author INSERT YOUR NAME HERE
+ */
+
 public class AssessmentRecord {
 
 }
